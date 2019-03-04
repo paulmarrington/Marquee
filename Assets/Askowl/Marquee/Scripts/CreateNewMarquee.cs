da8113a42a78414388091e21b01a16091f5fee92
paulmarrington <paul@marrington.net>
2019-03-04 10:56:38 +1000

    asstbuilder

--- Assets/Askowl/Marquee/Scripts/CreateNewMarquee.cs
+++ Assets/Askowl/Marquee/Scripts/CreateNewMarquee.cs
@@ -1,10 +1,34 @@
 ﻿// Copyright 2019 (C) paul@marrington.net http://www.askowl.net/unity-packages
 
+using CustomAsset.Constant;
+using CustomAsset.Mutable;
 using UnityEditor;
+using Integer = CustomAsset.Mutable.Integer;
+using String = CustomAsset.Mutable.String;
 
 namespace Askowl {
   /// <a href=""></a> //#TBD#//
-  public class CreateNewMarquee {
-    [MenuItem("Assets/Create/Marquee")] private void CreateMarquee() => AssetBuilder.CreateAssets("Marquee");
+  public class CreateNewMarquee : AssetBuilder {
+    [MenuItem("Assets/Create/Marquee")] private static void CreateMarquee() =>
+      new CreateNewMarquee().CreateAssets("Marquee");
+
+    protected override void OnScriptReload() {
+      CreateAssetDictionary(
+        ("TickertapeManager", typeof(Tickertape))
+      , ("Characters per second", typeof(Integer))
+      , ("Contents", typeof(Quotes))
+      , ("Display complete", typeof(Trigger))
+      , ("Now showing", typeof(String))
+      , ("Repeats per message", typeof(Integer)));
+
+      SetField("TickertapeManager", "quotes",          Asset("Contents"));
+      SetField("TickertapeManager", "showing",         Asset("Now showing"));
+      SetField("TickertapeManager", "showingComplete", Asset("Display complete"));
+
+      SetField("Marquee", "charactersPerSecond", Asset("Characters per second"));
+      SetField("Marquee", "repeats",             Asset("Repeats per message"));
+      SetField("Marquee", "showing",             Asset("Now showing"));
+      SetField("Marquee", "showingComplete",     Asset("Display complete"));
+    }
   }
 }
